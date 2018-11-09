9b3f1105186f1c46c8c8ec93af82012010ee6cd3
Alan Richardson <alan@compendiumdev.co.uk>
2018-11-09 14:25:04 +0000

    snakes array

---
+++ src/test/java/com/javafortesters/arrays/ArraysTest.java
@@ -0,0 +1,41 @@
+package com.javafortesters.arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ArraysTest {
+
+    private class Snake{
+        int length=0;
+    }
+
+
+
+    private class Board{
+
+        private Snake[] snakes;
+
+        Board(int numberOfSnakes){
+            this.snakes = new Snake[numberOfSnakes];
+        }
+    }
+
+    @Test
+    public void whenICreateABoardTheArrayIsCreated(){
+
+        Board withSixSnakes = new Board(6);
+        Assert.assertEquals(6, withSixSnakes.snakes.length);
+    }
+
+    @Test
+    public void butThereAreNoSnakesOnTheBoard(){
+
+        Board withSixSnakes = new Board(3);
+        Assert.assertEquals(3, withSixSnakes.snakes.length);
+        Assert.assertNull(withSixSnakes.snakes[0]);
+        Assert.assertNull(withSixSnakes.snakes[1]);
+        Assert.assertNull(withSixSnakes.snakes[2]);
+
+    }
+
+}
